5cc2b13100cd9b51b77913bbed4daae4532dca73
Jean <jean.crochette@gmail.com>
2021-04-23 14:13:00 +0200

    timercook  fonctionnel

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // Le temps restant
+  public timeLeftInSeconds = 0;
+  // la durée de cuisson
+  private durationInseconds = 4 * 60;
+// le chrono
+  public timer = null;
+
 
   constructor() {}
+
+    public startTimer(){
+      // unique qaund on lance un nouveau chrono
+      // et pas quand on reprend le lancement du chrono
+      if(this.timeLeftInSeconds == 0){
+          // le temps restant et egal a la durée
+      this.timeLeftInSeconds = this.durationInseconds;
+
+      }
+     
+      // a chaque seconde on decremente le temps restant
+      this.timer = setInterval(
+        ()=>{
+          this.timeLeftInSeconds--;
+          // ici on arrete le chrono quand le temps restant est egal a zero
+          if(this.timeLeftInSeconds == 0){
+            clearInterval(this.timer);
+            this.timer = null;
+          }
+        },
+        1000
+      );
+    }
+
+      public dispalayTime(timeInseconds){
+        let minutes = Math.floor(timeInseconds/60);
+        let seconds = timeInseconds % 60;
+
+             return minutes + ':' + seconds;
+      }
+      public stopTimer(){
+        clearInterval(this.timer);
+        this.timeLeftInSeconds = 0;
+        this.timer = null
+      }
+
+      public pauseTimer(){
+        clearInterval(this.timer);
+        this.timer = null;
+      }
 
 }
